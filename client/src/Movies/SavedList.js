8fb89e508d3ec0458b3dea396ae93d0b810816dc
Adam <adammonast@Adams-MBP.attlocal.net>
2019-12-18 08:26:17 -0500

    Finished movies project

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 
-const SavedList = props => (
-  <div className="saved-list">
+// const SavedList = props => (
+//   <div className="saved-list">
+//     <h3>Saved Movies:</h3>
+//     {props.list.map(movie => (
+//       <span className="saved-movie">{movie.title}</span>
+//       ))}
+//     <button className="home-button">Home</button>
+//   </div>
+// );
+
+// export default SavedList;
+
+const SavedList = props => {
+  const history = useHistory();
+  console.log(history);
+  // console.log(props);
+
+  const routeToHome = () => {
+    console.log("Loading...");
+    history.push("/");
+  };
+
+  return(
+    <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
-    ))}
-    <div className="home-button">Home</div>
+      ))}
+    <button onClick={routeToHome} className="home-button">Home</button>
   </div>
-);
+)}
 
-export default SavedList;
+export default SavedList
